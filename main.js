3a3a42f8ba7d26b024dfb2d6baa0b293de2d696c
viculocoding2025-sys <viculo.coding2025@gmail.com>
2025-11-24 15:27:27 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+const translations ={
+    'fa' : {
+        'title' : 'PulseCheck',
+        'brand' : 'PulseCheck',
+        'login' : 'ورود',
+        'works' : 'نحوه کار' ,
+        'pricing' : 'قیمت گذاری' ,
+        'language' : 'زبان',
+    }
+}
 
+function applyTranslations() {
+    
+    const elements = document.querySelectorAll('[data-i18n]');
+
+    const currentLang = 'fa';
+
+    elements.forEach(element => {
+
+        const key = element.getAttribute ('data-i18n');
+        if (translations[currentLang] && translations[currentLang][key]) {
+
+            const translatedText = translations[currentLang][key];
+            element.innerText = translatedText;
+        }
+    })
+}
